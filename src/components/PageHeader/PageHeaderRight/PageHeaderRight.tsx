c7986f5c1f7263feffe880732c369b078d85c39d
Ajrdn <ajrdn1276@gmail.com>
2023-11-02 16:56:20 +0900

    Init: Create PageHeaderRight Component

---
+++ src/components/PageHeader/PageHeaderRight/PageHeaderRight.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { styled } from '@mui/material/styles'
+import Box from '@mui/material/Box'
+import PageRouterButtonBox from './PageRouterButtonBox'
+
+
+const PageHeaderRightBox = styled(Box)({
+  width: '332px',
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+})
+
+
+function PageHeaderRight() {
+  return (
+    <PageHeaderRightBox>
+      <PageRouterButtonBox />
+    </PageHeaderRightBox>
+  )
+}
+
+export default PageHeaderRight
