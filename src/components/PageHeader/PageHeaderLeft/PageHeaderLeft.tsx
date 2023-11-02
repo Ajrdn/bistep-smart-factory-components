d12c93031b050747e4d3ee13e876ae49c7cd60f7
Ajrdn <ajrdn1276@gmail.com>
2023-11-02 16:18:01 +0900

    Init: Create PageHeaderLeft Component

---
+++ src/components/PageHeader/PageHeaderLeft/PageHeaderLeft.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { styled } from '@mui/material/styles'
+import Box from '@mui/material/Box'
+import TerenzLogo from './TerenzLogo'
+import PageRouters from './PageRouters'
+
+
+const PageHeaderLeftBox = styled(Box)({
+  display: 'flex',
+  alignItems: 'center',
+})
+
+
+function PageHeaderLeft() {
+  return (
+    <PageHeaderLeftBox>
+      <TerenzLogo />
+      <PageRouters />
+    </PageHeaderLeftBox>
+  )
+}
+
+export default PageHeaderLeft
