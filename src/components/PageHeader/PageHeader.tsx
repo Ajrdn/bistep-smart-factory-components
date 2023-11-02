44120711363ede48c4dfbd8d741f158eaf5367ad
Ajrdn <ajrdn1276@gmail.com>
2023-11-02 14:45:43 +0900

    Init: Create PageHeader Component

---
+++ src/components/PageHeader/PageHeader.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { styled } from '@mui/material/styles'
+import Box from '@mui/material/Box'
+import PageHeaderLeft from './PageHeaderLeft/PageHeaderLeft'
+import PageHeaderRight from './PageHeaderRight/PageHeaderRight'
+
+
+const PageHeaderBackground = styled(Box)(({ theme }) =>({
+  width: '1920px', 
+  height: '64px',
+  backgroundColor: theme.palette.primary.main,
+  display: 'flex',
+  alignItems: 'center',
+}))
+
+
+function PageHeader() {
+  return (
+    <PageHeaderBackground>
+      <PageHeaderLeft />
+      <PageHeaderRight />
+    </PageHeaderBackground>
+  )
+}
+
+export default PageHeader
