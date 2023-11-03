f200426a60e1e938a6c038b885f05746b79bb84e
Ajrdn <ajrdn1276@gmail.com>
2023-11-03 14:55:40 +0900

    Style: Add ';'

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { RouterProvider } from 'react-router-dom'
+import { RouterProvider } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
