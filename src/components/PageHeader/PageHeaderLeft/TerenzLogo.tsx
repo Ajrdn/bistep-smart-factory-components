e6218af11403d075a2d6c6d33fd2fe5b449c5db8
Ajrdn <ajrdn1276@gmail.com>
2023-11-03 14:01:40 +0900

    Feat: Import useNavigate from 'react-router-dom'

--- src/components/PageHeader/PageHeaderLeft/TerenzLogo.tsx
+++ src/components/PageHeader/PageHeaderLeft/TerenzLogo.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import Box from '@mui/material/Box'
 import { ReactComponent as Icon } from 'assets/images/terenz_logo.svg'
 
 
 function TerenzLogo() {
+  const navigate = useNavigate()
+
   return (
     <Box sx={{
       margin: '0 44px',
@@ -11,6 +14,7 @@ function TerenzLogo() {
       <Icon
         width='112px'
         height='44px'
+        onClick={() => navigate('/dashboard')}
       />
     </Box>
   )
