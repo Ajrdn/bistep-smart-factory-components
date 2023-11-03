211d9bac128f5efef2027ed8811533e8f6490a96
Ajrdn <ajrdn1276@gmail.com>
2023-11-03 10:31:44 +0900

    Feat: Add other routers

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,11 +1,46 @@
 import { createBrowserRouter } from 'react-router-dom'
 import Root from './Root'
+import Melting from './Melting/Melting'
+import Element from './Element/Element'
+import Dashboard from './Dashboard/Dashboard'
+import ProcessOrder from './ProcessOrder/ProcessOrder'
+import ProcessPlanner from './ProcessPlanner/ProcessPlanner'
+import ProcessList from './ProcessList/ProcessList'
+import Admin from './Admin/Admin'
 
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
+  },
+  {
+    path: '/melting',
+    element: <Melting />,
+  },
+  {
+    path: '/element',
+    element: <Element />,
+  },
+  {
+    path: '/dashboard',
+    element: <Dashboard />,
+  },
+  {
+    path: '/processOrder',
+    element: <ProcessOrder />,
+  },
+  {
+    path: '/processPlanner',
+    element: <ProcessPlanner />,
+  },
+  {
+    path: '/processList',
+    element: <ProcessList />,
+  },
+  {
+    path: '/admin',
+    element: <Admin />,
   },
 ])
 
