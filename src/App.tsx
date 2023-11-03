1bcec56f08161207aee6163326c76f3d71541515
Ajrdn <ajrdn1276@gmail.com>
2023-11-03 10:27:34 +0900

    Feat: Add RouterProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
+import { RouterProvider } from 'react-router-dom'
 import { styled } from '@mui/material/styles'
 import Box from '@mui/material/Box'
 import PageHeader from './components/PageHeader/PageHeader'
+import router from 'routes/router'
 
 
 const PageBackground = styled(Box)({
   width: '100vw',
   height: '100vh',
   display: 'flex',
-  justifyContent: 'center',
+  flexDirection: 'column',
+  alignItems: 'center',
 })
 
 
@@ -16,6 +19,7 @@ function App() {
   return (
     <PageBackground>
       <PageHeader />
+      <RouterProvider router={router} />
     </PageBackground>
   )
 }
