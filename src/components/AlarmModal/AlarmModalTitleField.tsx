b1f22152060a2aadfd36edfc55de1839abe8329d
Ajrdn <ajrdn1276@gmail.com>
2023-11-06 09:47:24 +0900

    Style: Add 'styled' from '@mui/material/styles' to make TitleField Component

--- src/components/AlarmModal/AlarmModalTitleField.tsx
+++ src/components/AlarmModal/AlarmModalTitleField.tsx
@@ -1,6 +1,25 @@
 import React from 'react'
 import useAlarmModalStore from 'store/useAlarmModalStore'
+import { styled } from '@mui/material/styles'
 import TextField from '@mui/material/TextField'
+
+
+const TitleField = styled(TextField)({
+  width: '100%',
+  '& .MuiInputBase-root': {
+    height: '51px',
+    padding: '15px 20px',
+    borderRadius: '4px',
+    marginBottom: '10px',
+  },
+  '& input': {
+    padding: 0,
+    fontSize: '18px',
+    fontFamily: 'Pretendard',
+    fontWeight: 600,
+    color: '#C8C8C8',
+  },
+})
 
 
 function AlarmModalTitleField() {
@@ -12,27 +31,11 @@ function AlarmModalTitleField() {
   }
 
   return (
-    <TextField
+    <TitleField
       autoFocus
       placeholder='제목 입력'
       value={title}
       onChange={titleChange}
-      sx={{
-        width: '100%',
-        '& .MuiInputBase-root': {
-          height: '51px',
-          padding: '15px 20px',
-          borderRadius: '4px',
-          marginBottom: '10px',
-        },
-        '& input': {
-          padding: 0,
-          fontSize: '18px',
-          fontFamily: 'Pretendard',
-          fontWeight: 600,
-          color: '#C8C8C8',
-        },
-      }}
     />
   )
 }
