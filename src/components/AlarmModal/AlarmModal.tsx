65bc91fea83f59943b6723d13fa47a4c79557be3
Ajrdn <ajrdn1276@gmail.com>
2023-11-06 10:08:15 +0900

    Rename: Update imported components path

---
+++ src/components/AlarmModal/AlarmModal.tsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import useAlarmModalStore from 'store/useAlarmModalStore'
+import { styled } from '@mui/material/styles'
+import ModalBackground from 'components/ModalComponent/ModalBackground'
+import ModalContentBox from 'components/ModalComponent/ModalContentBox'
+import ModalCloseButton from 'components/ModalComponent/ModalCloseButton'
+import ModalTitle from 'components/ModalComponent/ModalTitle'
+import AlarmModalTitleField from './AlarmModalTitleField'
+import AlarmModalContentField from './AlarmModalContentField'
+import AlarmModalBottomField from './AlarmModaBottomField/AlarmModalBottomField'
+
+
+const AlarmModalContentBox = styled(ModalContentBox)({
+  borderRadius: '25px',
+  backgroundColor: 'white',
+  width: '800px',
+  height: '592px',
+  padding: '60px 50px 70px',
+})
+
+
+const AlarmModalTitle = styled(ModalTitle)({
+  marginBottom: '40px',
+})
+
+
+function AlarmModal() {
+  const open = useAlarmModalStore(state => state.open)
+  const setOpen = useAlarmModalStore(state => state.setOpen)
+  const setTitle = useAlarmModalStore(state => state.setTitle)
+  const setContent = useAlarmModalStore(state => state.setContent)
+  const setTarget = useAlarmModalStore(state => state.setTarget)
+
+  const setClose = () => {
+    setTitle('')
+    setContent('')
+    setTarget('전체 공정')
+    setOpen()
+  }
+
+  return (
+    <ModalBackground open={open} onClose={setClose}>
+      <AlarmModalContentBox>
+        <AlarmModalTitle>알림 보내기</AlarmModalTitle>
+        <AlarmModalTitleField />
+        <AlarmModalContentField />
+        <AlarmModalBottomField />
+        <ModalCloseButton
+          size='30px'
+          position='10px'
+          color='#878787'
+          backgroundColor='#EFEFEF'
+          onClick={setClose}
+        />
+      </AlarmModalContentBox>
+    </ModalBackground>
+  )
+}
+
+export default AlarmModal
