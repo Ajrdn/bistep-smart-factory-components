3df7cc7666c4be9137b76e5db9f69ede0e9c3eee
Ajrdn <ajrdn1276@gmail.com>
2023-11-06 09:40:26 +0900

    Init: Create ModalTitle Component

---
+++ src/components/ModalComponent/ModalTitle.tsx
@@ -0,0 +1,13 @@
+import { styled } from '@mui/material/styles'
+import Typography from '@mui/material/Typography'
+
+
+const ModalTitle = styled(Typography)({
+  color: '#13243A',
+  fontSize: '23px',
+  fontFamily: 'Pretendard',
+  fontWeight: 700,
+  textAlign: 'center',
+})
+
+export default ModalTitle
