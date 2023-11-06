41959c7a27f0ff2f34dd58a66742a6da88a8067e
Ajrdn <ajrdn1276@gmail.com>
2023-11-06 16:54:48 +0900

    Design: Update TextField focused border style

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,10 +1,22 @@
 import { createTheme } from '@mui/material/styles'
+import { outlinedInputClasses } from '@mui/material/OutlinedInput'
 
 
 export const theme = createTheme({
   palette: {
     primary: {
       main: '#13243A',
+    },
+  },
+  components: {
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: {
+          [`&.Mui-focused .${outlinedInputClasses.notchedOutline}`]: {
+            border: '1px solid #C8C8C8',
+          },
+        },
+      },
     },
   },
 })
