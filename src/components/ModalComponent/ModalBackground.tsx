6539704cb636a55609528728f347d78840241dd6
Ajrdn <ajrdn1276@gmail.com>
2023-11-06 09:39:48 +0900

    Init: Create ModalBackground Component

---
+++ src/components/ModalComponent/ModalBackground.tsx
@@ -0,0 +1,13 @@
+import { styled } from '@mui/material/styles'
+import Modal from '@mui/material/Modal'
+
+
+const ModalBackground = styled(Modal)({
+  backdropFilter: 'blur(3px)',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+})
+
+
+export default ModalBackground
