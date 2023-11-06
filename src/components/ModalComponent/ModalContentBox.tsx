fb05f93f0c91dcea5d3bf5fb0dfb30b44403a0ed
Ajrdn <ajrdn1276@gmail.com>
2023-11-06 09:41:46 +0900

    Init: Create ModalContentBox Component

---
+++ src/components/ModalComponent/ModalContentBox.tsx
@@ -0,0 +1,12 @@
+import { styled } from '@mui/material/styles'
+import Box from '@mui/material/Box'
+
+
+const ModalContentBox = styled(Box)({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  position: 'relative',
+})
+
+export default ModalContentBox
